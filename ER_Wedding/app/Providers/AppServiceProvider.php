87aa948bd9f14e42e360c60428ce280b39dd4071
nikennaa <nikensuhisman@gmail.com>
2025-06-03 02:35:48 +0700

    update appserviceprovider untuk deployment

--- ER_Wedding/app/Providers/AppServiceProvider.php
+++ ER_Wedding/app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //agar link nya https
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+
     }
 }
